850e948523a7fd9fb4517ab9259f2eef4d9e39a0
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-08 09:10:13 +0530

    UserApp with React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddUser from './Components/AddUser';
+import SearchUser from './Components/SearchUser';
+import DeleteUser from './Components/DeleteUser';
+import ViewAll from './Components/ViewAll';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AddUser/>}/>
+      <Route path="/search" element={<SearchUser/>}/>
+      <Route path="/delete" element={<DeleteUser/>}/>
+      <Route path="/view" element={<ViewAll/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
